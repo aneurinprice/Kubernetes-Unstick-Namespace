79357e5fd74e201c3b7c60784cce560049a7308c
Aneurin Price <adp@nyeprice.space>
2020-09-24 14:48:27 +0100

    aa

--- namespace-cleanup.sh
+++ namespace-cleanup.sh
@@ -2,3 +2,22 @@ NAMESPACE=$1
 kubectl proxy &
 kubectl get namespace $NAMESPACE -o json |jq '.spec = {"finalizers":[]}' >temp.json
 curl -k -H "Content-Type: application/json" -X PUT --data-binary @temp.json 127.0.0.1:8001/api/v1/namespaces/$NAMESPACE/finalize
+
+sleep 5
+
+if kubectl get namespace $NAMESPACE
+then
+	kubectl patch namespace $NAMESPACE -p '{"metadata":{"finalizers": []}}' --type=merge
+else
+	echo "Namespace deleted"
+	exit 0
+fi
+
+if kubectl get namespace $NAMESPACE
+then
+	echo "I have failed you"
+	exit 1
+else
+	echo "Namespace deleted"
+	exit 0
+fi
